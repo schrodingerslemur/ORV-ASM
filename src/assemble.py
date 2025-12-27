e5d3ae6cd21848982ef079c9a80f41a8d853646c
Brendan Hendrata <bhendrata@cmu.edu>
2025-12-27 04:20:29 -0500

    implemented check_imm

--- src/assemble.py
+++ src/assemble.py
@@ -63,6 +63,7 @@ def assemble_line(
     opcode_type = get_opcode_type(op)
 
     if opcode_type == 'PSEUDO':
+        # TODO: Handle pseudo-instructions (and everything in this block)
         args = get_pseudo_args(op, non_op, metadata)
         for pseudo_inst in pseudo[op]:
             pseudo_op, pseudo_non_op = parse_op(pseudo_inst)
@@ -70,6 +71,7 @@ def assemble_line(
             instructions = get_instruction(pseudo_op, pseudo_non_op, pseudo_opcode, pseudo_opcode_type, metadata)
             return instructions # list of instructions
     else:
+        # Normal instruction
         args = get_args(op, non_op, opcode_type, metadata)
         instruction = get_instruction(opcode_type, args)
         return instruction
@@ -79,10 +81,11 @@ def get_args(
     non_op: str,
     opcode_type: str,
     metadata: dict,
-) -> list[str]:
+) -> list[str]: # include opcode too
     """Parses the all the parts into arguments based on opcode type."""
     args = [arg.strip() for arg in non_op.split(',')]
 
+    # Valid number of arguments
     if opcode_type in ['R', 'I', 'SI', 'JI', 'B']:
         if len(args) != 3:
             raise InvalidArgumentError(f"Invalid number of arguments for {opcode_type}-type instruction: {op} {non_op}")
@@ -90,8 +93,33 @@ def get_args(
     elif opcode_type in ['LI', 'S']:
         if len(args) != 2:
             raise InvalidArgumentError(f"Invalid number of arguments for {opcode_type}-type instruction: {op} {non_op}")
-        
+
+    # Modify for S-type and LI-type
+    if opcode_type in ['S', 'LI']:
+        # args: rs2, offset(rs1)
+        match = re.match(r'(-?\d+)\s*\(\s*(r[0-9]|1[0-9]|2[0-9]|3[0-1])\s*\)', args[1])
+        if not match:
+            raise InvalidArgumentError(f"Invalid S-type or LI-type argument format: {args[1]}")
+        offset = match.group(1)
+        rs1 = match.group(2)
+        args = [args[0], rs1, offset]  # rs2, rs1, offset
+
+        return [opcode[op][0]] + [get_register(args[0]), get_register(args[1]), get_imm(args[2])]
+    
+
+
+
     return args
+
+def check_imm(
+    imm_str: str
+) -> str:
+    """Converts an immediate string to its integer representation."""
+    try:
+        _ = int(imm_str)
+    except ValueError:
+        raise InvalidArgumentError(f"Invalid immediate value: {imm_str}")
+    return imm_str
 
 def get_register(
     reg_str: str
